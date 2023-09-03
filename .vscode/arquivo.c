1d17655799ede85c8ff64879f1da861cb57516f0
mateusgts007 <mateusgtsandrade@gmail.com>
2023-09-03 11:10:40 -0300

    teste

---
+++ .vscode/arquivo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+
+int x;
+
+printf("porfavor digite um numero:\n ");
+scanf("%d",&x);
+
+while (x>=0){
+    printf("%d",x);
+    x++;
+
+    if (x>= 10){
+        break;
+    
+    }
+}
+
+
+
+
+
+
+// / //sadnsdionaodnsa
+
+
+    return 0;
+}
